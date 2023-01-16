463a9e8360965059eb9354bd754a1022842d51fc
Mohamad Sleiman Haidar <mhaidar@myki.com>
2023-01-16 17:58:17 +0300

    add noon page and increase width of main containers

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <Suspense fallback={null}>
       <Container>
-        <div className="flex flex-col justify-center items-start max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pb-16">
+        <div className="flex flex-col justify-center items-start max-w-4xl 2xl:max-w-2xl  border-gray-200 dark:border-gray-700 mx-auto pb-16 ">
           <div className="flex flex-col-reverse sm:flex-row items-start">
             <div className="flex flex-col pr-8">
               <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white">
@@ -20,8 +20,21 @@ export default function Home() {
                 {/*<span className="font-semibold">Vercel</span>*/}
               </h2>
               <p className="text-gray-600 dark:text-gray-400 mb-16">
-                Full stack developer with over 12 years of experience building websites, services and platforms. Constantly tinkering and prototyping with new methodologies, technologies, languages and libraries. Able to architect and engineer the whole development stack from the front facing clients, to the APIs, the back-end services to the provisioning and configuring of the network infrastructure. I
-                rely almost entirely on open source and modern resources and libraries. The experience I gained handling active back-end services used by hundreds of thousands of active users enables me to architect and build back-end services which are wholly extensible and can scale. My freelance work over the years has enabled me to tinker with a vast array of different tools and libraries which I used to build everything from simple front facing portfolio sites to complex, real-time and entirely reactive and state driven client platforms.
+                Full stack developer with over 12 years of experience building
+                websites, services and platforms. Constantly tinkering and
+                prototyping with new methodologies, technologies, languages and
+                libraries. Able to architect and engineer the whole development
+                stack from the front facing clients, to the APIs, the back-end
+                services to the provisioning and configuring of the network
+                infrastructure. I rely almost entirely on open source and modern
+                resources and libraries. The experience I gained handling active
+                back-end services used by hundreds of thousands of active users
+                enables me to architect and build back-end services which are
+                wholly extensible and can scale. My freelance work over the
+                years has enabled me to tinker with a vast array of different
+                tools and libraries which I used to build everything from simple
+                front facing portfolio sites to complex, real-time and entirely
+                reactive and state driven client platforms.
               </p>
             </div>
             {/*<div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto">*/}
--- pages/noon.tsx
+++ pages/noon.tsx
@@ -11,60 +11,58 @@ export default function Noon() {
 
   return (
     <Container
-      title="Blog – Lee Robinson"
-      description="Thoughts on the software industry, programming, tech, videography, music, and my personal life."
+      title="Noon – Open Source Communication Platform"
+      description="Noon is a free, open source, secure communication platform. For anyone interested in having their own secure communication platform that internalizes all data and communication and does not rely on any third party service or package."
     >
-      <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
+      <div className="flex flex-col items-start justify-center max-w-4xl 2xl:max-w-4xl mx-auto mb-16">
         <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
-          Blog
+          Noon
         </h1>
 
         <p className="mb-4 text-gray-600 dark:text-gray-400">
-          {`I've been writing online since 2014, mostly about web development and tech careers.
-            In total, I've written  articles on my blog.
-            Use the search below to filter by title.`}
+          {`Noon is a free, open source, secure communication platform. For anyone interested in having their own secure communication platform that internalizes all data and communication and does not rely on any third party service or package.`}
         </p>
 
-        <div className="relative w-full mb-4">
-          <input
-            aria-label="Search articles"
-            type="text"
-            onChange={(e) => setSearchValue(e.target.value)}
-            placeholder="Search articles"
-            className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-200 rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:text-gray-100"
-          />
+        <p className="mb-4 text-gray-600 dark:text-gray-400">
+          {`In the following sections, I will attempt to explain the various parts
+          that make up the platform.`}
+        </p>
 
-          <svg
-            className="absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </div>
+        {/*<div className="relative w-full mb-4">*/}
+        {/*  <svg*/}
+        {/*    className="absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"*/}
+        {/*    xmlns="http://www.w3.org/2000/svg"*/}
+        {/*    fill="none"*/}
+        {/*    viewBox="0 0 24 24"*/}
+        {/*    stroke="currentColor"*/}
+        {/*  >*/}
+        {/*    <path*/}
+        {/*      strokeLinecap="round"*/}
+        {/*      strokeLinejoin="round"*/}
+        {/*      strokeWidth={2}*/}
+        {/*      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"*/}
+        {/*    />*/}
+        {/*  </svg>*/}
+        {/*</div>*/}
 
         {!searchValue && (
           <>
             <h3 className="mt-8 mb-4 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
-              Most Popular
+              Back-end
             </h3>
+
             <BlogPost
               title="Rust Is The Future of JavaScript Infrastructure"
               excerpt="Why is Rust being used to replace parts of the JavaScript web ecosystem like minification (Terser), transpilation (Babel), formatting (Prettier), bundling (webpack), linting (ESLint), and more?"
               slug="rust"
             />
+
             <BlogPost
               title="Everything I Know About Style Guides, Design Systems, and Component Libraries"
               excerpt="A deep-dive on everything I've learned in the past year building style guides, design systems, component libraries, and their best practices."
               slug="style-guides-component-libraries-design-systems"
             />
+
             <BlogPost
               title="Building a Design System Monorepo with Turborepo"
               excerpt="Manage multiple packages with a shared build, test, and release process using Turborepo, Changesets, Storybook, and more."
